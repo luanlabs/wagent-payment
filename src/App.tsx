761de3652bd5eeb35f0ef45319c952d0655a187f
Ruveyda-Kışla <ruveydakisla572@gmail.com>
2024-05-17 22:48:50 +0330

    feat: add vite to project

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-normal">Wagent</h1>
+      <h1 className="text-3xl font-medium">Wagent</h1>
+      <h1 className="text-3xl font-semibold">Wagent</h1>
+    </>
+  );
+}
