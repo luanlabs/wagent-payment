d864659e03e58b68172bad918e9f2122a3cb62cf
dousdogukan <dousdogukan@gmail.com>
2024-05-24 02:42:50 +0330

    feat: implement CButton

--- src/Components/CButton/index.tsx
+++ src/Components/CButton/index.tsx
@@ -6,7 +6,7 @@ interface CButtonProps {
 const CButton = ({ text, onClick }: CButtonProps) => {
   return (
     <button
-      className="w-full font-medium px-2 py-2 text-black text-base text-center border border-1 border-[#D0D5DD] rounded-lg hover:bg-[#F9FAFB] "
+      className="w-full font-medium px-2 py-2 text-black text-base text-center border border-1 border-[#D0D5DD] rounded-lg hover:bg-[#F9FAFB] transition active:shadow"
       onClick={onClick}
     >
       {text}
