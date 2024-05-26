c7d887254e7c5cfc8c317f9f1a6e2e15445eefe0
dousdogukan <dousdogukan@gmail.com>
2024-05-26 20:14:44 +0330

    chore: add variant to CButton

--- src/Components/CButton/index.tsx
+++ src/Components/CButton/index.tsx
@@ -1,15 +1,24 @@
+export type Variant = 'simple' | 'bordered';
 interface CButtonProps {
-  text: string;
+  text?: string;
   onClick?: () => void;
+  variant: Variant;
+  className?: string;
+  children?: React.ReactNode;
 }
 
-const CButton = ({ text, onClick }: CButtonProps) => {
+const CButton = ({ text, onClick, variant, className, children }: CButtonProps) => {
+  const buttonStyle =
+    variant === 'bordered'
+      ? 'border border-1 border-[#D0D5DD] text-base font-medium hover:bg-[#F9FAFB] active:shadow'
+      : className;
+
   return (
     <button
-      className="w-full font-medium px-2 py-2 text-black text-base text-center border border-1 border-[#D0D5DD] rounded-lg hover:bg-[#F9FAFB] transition active:shadow"
+      className={`w-full px-2 py-2 text-black text-center cursor-pointer rounded-lg transition ${buttonStyle} ${className}`}
       onClick={onClick}
     >
-      {text}
+      {text ? text : children}
     </button>
   );
 };
