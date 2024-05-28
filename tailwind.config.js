58ac12661fd8affaf4e8d9983aad29173a4c8186
dousdogukan <dousdogukan@gmail.com>
2024-05-28 23:01:09 +0330

    chore: add some colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        lightGray: '#F2F4F7',
+        darkBlue: '#101828',
+        mediumGray: '#98A2B3',
+        offWhite: '#FCFCFD',
+      },
+    },
   },
   plugins: [],
 };
