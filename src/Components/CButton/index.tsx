ddcb9820e2797c7809f206904a5162e64540ff7d
dousdogukan <dousdogukan@gmail.com>
2024-06-06 18:30:48 +0330

    fix: fix button hover

--- src/Components/CButton/index.tsx
+++ src/Components/CButton/index.tsx
@@ -13,14 +13,14 @@ const CButton = ({ text, onClick, variant, className, children }: CButtonProps) 
   return (
     <button
       className={clsx(
-        `w-full px-2 py-2 text-center cursor-pointer rounded-lg transition`,
+        `w-full px-2 py-2 text-center cursor-pointer rounded-lg transition duration-300`,
         className,
         {
-          'border border-1 text-darkBlue border-gray text-base font-medium hover:bg-customGray active:shadow transition-all duration-700':
+          'border border-1 text-darkBlue border-gray text-base font-medium hover:bg-lightestGray active:shadow':
             variant === 'bordered',
         },
         {
-          'bg-emeraldGreen text-white hover:bg-primaryGreen/90 active:bg-primaryGreen transition-all duration-700':
+          'bg-emeraldGreen text-white hover:bg-emeraldGreen/90 active:bg-emeraldGreen':
             variant === 'confirm',
         },
       )}
