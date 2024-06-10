88bf21a57b3105c4353d0829370f8fc189003cc6
dousdogukan <dousdogukan@gmail.com>
2024-06-11 00:52:59 +0330

    fix: fix radius

--- src/Components/CButton/index.tsx
+++ src/Components/CButton/index.tsx
@@ -19,16 +19,12 @@ const CButton = ({ text, onClick, variant, className, children }: CButtonProps) 
   return (
     <button
       className={clsx(
-        `w-full px-2 py-2 text-center cursor-pointer rounded-lg transition duration-300`,
+        `w-full px-2 py-2 text-center cursor-pointer rounded-[10px] transition duration-300`,
         className,
         {
           'border border-1 text-darkBlue border-gray text-base font-medium hover:bg-lightestGray active:shadow':
             variant === 'bordered',
         },
-        // {
-        //   'bg-emeraldGreen text-white hover:bg-emeraldGreen/90 active:bg-emeraldGreen':
-        //     variant === 'confirm',
-        // },
       )}
       onClick={onClick}
     >
--- src/Components/CRadioButtonGroup/index.tsx
+++ src/Components/CRadioButtonGroup/index.tsx
@@ -40,7 +40,7 @@ const CRadioButtonGroup = ({ tabs, defaultSelectedTab, onChange }: CRadioButtonG
 
             <div
               className={clsx(
-                'py-2 cursor-pointer rounded-[10px] transition-all duration-300 select-none text-base border border-transparent',
+                'py-2 cursor-pointer rounded-[7px] transition-all duration-300 select-none text-base border border-transparent',
                 {
                   'bg-white border border-1 !border-customGray transition-all duration-300':
                     selectedTab === tab.value,
