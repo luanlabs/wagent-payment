377af82b904b003e5f9b8f0646896e3e197830db
dousdogukan <dousdogukan@gmail.com>
2024-06-12 02:50:13 +0330

    chore: add valueColor for total amount color text

--- src/Components/CResultDetail/index.tsx
+++ src/Components/CResultDetail/index.tsx
@@ -1,13 +1,14 @@
 type CResultDetailProps = {
   label: string;
   value: string | React.ReactNode;
+  valueColor?: string;
 };
 
-const CResultDetail = ({ label, value }: CResultDetailProps) => {
+const CResultDetail = ({ label, value, valueColor }: CResultDetailProps) => {
   return (
     <div className="between desktop:h-[18%] h-10 text-base font-medium">
       <p className="select-none">{label}:</p>
-      <p className="text-cadetBlue">{value}</p>
+      <p className={`text-cadetBlue ${valueColor}`}>{value}</p>
     </div>
   );
 };
