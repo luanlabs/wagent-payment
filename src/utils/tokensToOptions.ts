fd14c23e68ae5cbf71ef0b6c931a06222424dc99
dousdogukan <dousdogukan@gmail.com>
2024-09-15 18:24:05 +0330

    chore: add default token logo

--- src/utils/tokensToOptions.ts
+++ src/utils/tokensToOptions.ts
@@ -1,10 +1,12 @@
 import { ITokenResponse, OptionType } from '../models';
 
+import defaultTokenLogo from '../../public/images/defaultToken.svg';
+
 export const tokensToOptions = (tokens: ITokenResponse[]): OptionType[] => {
   const mappedTokens = tokens.map((token) => ({
     label: token.symbol.toUpperCase(),
     value: token.address,
-    logo: token.logo,
+    logo: token.logo ? token.logo : defaultTokenLogo,
   }));
   return mappedTokens;
 };
