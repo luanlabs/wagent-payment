008f05100c57e3cd355fb9351d467a3787602a22
dousdogukan <dousdogukan@gmail.com>
2024-09-16 21:09:11 +0330

    temp: remove / route

--- src/main.tsx
+++ src/main.tsx
@@ -9,7 +9,6 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Router>
       <Routes>
-        <Route path="/" element={<App />} />
         <Route path="/:id" element={<App />} />
       </Routes>
     </Router>
