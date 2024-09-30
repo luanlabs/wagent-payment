98b798b1545b24035b11b0307f68c926dce08b50
dousdogukan <dousdogukan@gmail.com>
2024-10-01 03:07:54 +0330

    temp: delete other networks

--- src/constants/networks.ts
+++ src/constants/networks.ts
@@ -1,23 +1,9 @@
-export const Futurenet = {
-  network: 'FUTURENET',
-  contract: '',
-  networkUrl: 'https://horizon-futurenet.stellar.org',
-  networkPassphrase: 'Test SDF Future Network ; October 2022',
-  sorobanRpcUrl: 'https://rpc-futurenet.stellar.org',
-};
-
-export const Mainnet = {
-  network: 'PUBLIC',
-  contract: 'CAWGWAQ566GGLHJFH3JRVMCFZLXR6SGU2CSAHPEDOUZDLV7C4MFHFBFP',
-  networkUrl: 'https://horizon.stellar.org',
-  networkPassphrase: 'Public Global Stellar Network ; September 2015',
-  sorobanRpcUrl: 'https://soroban-testnet.stellar.org', // TODO: change to actual mainnet RPC
-};
-
-export const Testnet = {
+const Testnet = {
   network: 'TESTNET',
-  contract: 'CAWGWAQ566GGLHJFH3JRVMCFZLXR6SGU2CSAHPEDOUZDLV7C4MFHFBFP',
+  contract: import.meta.env.VITE_CONTRACT_ADDRESS,
   networkUrl: 'https://horizon-testnet.stellar.org',
   networkPassphrase: 'Test SDF Network ; September 2015',
   sorobanRpcUrl: 'https://soroban-testnet.stellar.org',
 };
+
+export default Testnet;
