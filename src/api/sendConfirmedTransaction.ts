a4835c88142d363257d4675baafabfb8e18f1030
dousdogukan <dousdogukan@gmail.com>
2024-10-14 18:56:18 +0330

    fix: fix payer email

--- src/api/sendConfirmedTransaction.ts
+++ src/api/sendConfirmedTransaction.ts
@@ -1,15 +1,18 @@
 import fetcher from '../utils/fetcher';
 
 const sendConfirmedTransaction = async (id: string, hash: string, payerEmail: string) => {
+  const body: { hash: string; payerEmail?: string } = { hash };
+
+  if (payerEmail) {
+    body.payerEmail = payerEmail;
+  }
+
   await fetcher(import.meta.env.VITE_WAGENT_API + `/orders/${id}/transactions`, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
     },
-    body: JSON.stringify({
-      hash: hash,
-      payerEmail: payerEmail,
-    }),
+    body: JSON.stringify(body),
   });
 };
 
