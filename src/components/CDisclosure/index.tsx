7a6f3f9eaf29267f45e441a4732112599dfa5141
Ruveyda Kisla <ruveydakisla572@gmail.com>
2024-11-15 20:02:48 +0330

    refactor: change disclosure component

--- src/components/CDisclosure/index.tsx
+++ src/components/CDisclosure/index.tsx
@@ -5,7 +5,7 @@ interface CDisclosureProps {
   title: string;
   subTitle?: string;
   content: React.ReactNode | string;
-  icon?: React.ReactNode | string;
+  icon?: string | undefined;
   label?: string;
 }
 
@@ -15,35 +15,40 @@ const CDisclosure = ({ title, subTitle, content, icon, label }: CDisclosureProps
       {label && <h2 className="mb-4 text-2xl font-medium text-darkBlue">{label}</h2>}
       <div className="w-full rounded-xl bg-offWhite border border-1 border-customGray">
         <Disclosure as="div" className="py-6">
-          <DisclosureButton className="group w-full flex items-center px-4">
-            <div className="flex w-full">
-              {icon && (
-                <div className="bg-customGray flex items-center justify-center w-[57px] h-[50px] rounded-[10px] mr-3">
-                  {icon}
+          {({ open }) => (
+            <>
+              <DisclosureButton className="group w-full flex items-center px-4">
+                <div className="flex w-full bg-lightGray border rounded-lg border-customGray p-2.5">
+                  {icon && (
+                    <div className="flex items-center justify-center mr-3">
+                      <img src={icon} alt={title} className="rounded-lg" height={40} width={40} />
+                    </div>
+                  )}
+                  <div className="w-full flex flex-col justify-center">
+                    <div className="flex items-center justify-between w-full">
+                      <span className="text-lg mobile:text-base !font-[Aeonik-m] transition-colors group-data-[hover]:text-darkBlue/80">
+                        {title}
+                      </span>
+                      <BottomArrowIcon fill={open ? '#05DC91' : '#475467'} />
+                    </div>
+                    {subTitle && <p className="text-mediumGray text-base text-left">{subTitle}</p>}
+                  </div>
                 </div>
-              )}
-
-              <div className="w-full flex flex-col justify-center">
-                <div className="flex items-center justify-between w-full">
-                  <span className="text-lg font-medium text-red transition-colors group-data-[hover]:text-darkBlue/80">
-                    {title}
-                  </span>
-                  <BottomArrowIcon fill="#000" />
-                </div>
-                {subTitle && <p className="text-mediumGray text-base text-left">{subTitle}</p>}
-              </div>
-            </div>
-          </DisclosureButton>
-          <Transition
-            enter="duration-200 ease-out"
-            enterFrom="opacity-0 -translate-y-6"
-            enterTo="opacity-100 translate-y-0"
-            leave="duration-200 ease-out"
-            leaveFrom="opacity-100 translate-y-0"
-            leaveTo="opacity-0 -translate-y-6"
-          >
-            <DisclosurePanel className="mt-4 text-sm text-darkBlue px-2">{content}</DisclosurePanel>
-          </Transition>
+              </DisclosureButton>
+              <Transition
+                enter="duration-300 ease-in-out"
+                enterFrom="opacity-0 -translate-y-4"
+                enterTo="opacity-100 translate-y-0"
+                leave="duration-300 ease-in-out"
+                leaveFrom="opacity-100 translate-y-0"
+                leaveTo="opacity-0 -translate-y-4"
+              >
+                <DisclosurePanel className="mt-4 text-sm text-darkBlue px-4">
+                  {content}
+                </DisclosurePanel>
+              </Transition>
+            </>
+          )}
         </Disclosure>
       </div>
     </div>
