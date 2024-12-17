9468f7ad01900cb37cfdb69600bff89941265e00
dousdogukan <dousdogukan@gmail.com>
2024-12-17 21:16:11 +0330

    temp: remove timer from StepWrapper

--- src/components/StepWrapper/index.tsx
+++ src/components/StepWrapper/index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import clsx from 'clsx';
 
 import CButton from '../CButton';
-import CountdownTimer from '../CountdownTimer';
 
 interface StepWrapperProps {
   children: React.ReactNode;
@@ -11,7 +10,6 @@ interface StepWrapperProps {
   prevStep?: () => void;
   showNext?: boolean;
   showPrev?: boolean;
-  showCountdown?: boolean;
   disable?: boolean;
 }
 
@@ -23,20 +21,12 @@ const StepWrapper = ({
   showNext = true,
   showPrev = true,
   disable = false,
-  showCountdown = true,
 }: StepWrapperProps) => {
   return (
-    <div className="p-6 flex flex-col justify-between bg-white rounded-[12px] w-full h-full mobile:h-dvh  mobile:!pb-10 mobile:p-3">
-      <div>
+    <div className="p-6 flex flex-col justify-between bg-white rounded-[12px] w-full h-full mobile:p-0 mobile:bg-transparent">
+      <div className="mobile:bg-white rounded-[12px] mobile:p-3 mobile:mb-20 mobile:pb-10">
         <div className="flex justify-between relative">
           <h2 className="text-[22px] mobile:text-[18px] font-[Aeonik-m] mb-4">{title}</h2>
-          {showCountdown ? (
-            <div className="h-[60px] w-[100px] absolute right-0 -top-3">
-              <CountdownTimer expiredTimeStamp={1000} />
-            </div>
-          ) : (
-            ''
-          )}
         </div>
 
         <div>{children}</div>
@@ -45,7 +35,7 @@ const StepWrapper = ({
       <div
         className={clsx(
           'w-full flex items-center justify-end mt-4',
-          'mobile:fixed mobile:bottom-0 mobile:left-0 mobile:bg-white mobile:p-4 mobile:!w-full mobile:shadow-md z-30',
+          'mobile:fixed mobile:bottom-0 mobile:left-0 z-30 mobile:bg-white mobile:p-4 mobile:!w-full mobile:shadow-md',
           {
             '!justify-start': !showNext && showPrev,
           },
