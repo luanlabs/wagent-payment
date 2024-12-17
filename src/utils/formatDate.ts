be33c251802c01014e458e1e8599310f8d6b2cfd
dousdogukan <dousdogukan@gmail.com>
2024-12-17 21:14:08 +0330

    chore: add formatDate

---
+++ src/utils/formatDate.ts
@@ -0,0 +1,12 @@
+const formatDate = (date: number) => {
+  return new Date(date).toLocaleDateString('en-US', {
+    year: 'numeric',
+    month: 'short',
+    day: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
+    hour12: false,
+  });
+};
+
+export default formatDate;
