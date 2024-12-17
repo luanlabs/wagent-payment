a3da34154d09dc2b0d39029d606fa75247d76035
dousdogukan <dousdogukan@gmail.com>
2024-12-17 21:19:20 +0330

    chore: completed ExpiredBox

--- src/containers/ExpiredBox/index.tsx
+++ src/containers/ExpiredBox/index.tsx
@@ -1,10 +1,18 @@
 import counterClockwise from '/images/counterClockwise.svg';
 import rolling from '/images/rolling.svg';
 
-const ExpiredBox = () => {
+interface IExpiredBox {
+  redirectUrl: string;
+}
+
+const ExpiredBox = ({ redirectUrl }: IExpiredBox) => {
+  setInterval(() => {
+    window.location.href = redirectUrl;
+  }, 4000);
+
   return (
-    <div className="desktop:w-[404px] mobile:w-full mobile:mx-4 h-[278px]">
-      <div className="center flex-col">
+    <div className="desktop:w-[410px] mobile:w-full mobile:mx-4 h-[278px] px-4 bg-white shadow-box rounded-2xl">
+      <div className="center flex-col justify-center items-center h-full">
         <img src={counterClockwise} alt="canceled" draggable={false} />
         <p className="text-2xl mobile:text-xl text-[#D92D20] font-[Aeonik-m] mt-4">Order Expired</p>
         <p className="text-[#475467] text-center text-base my-2">
