f4c8de8b60f9f30effeb49d04883b1a3438c0ea0
dousdogukan <dousdogukan@gmail.com>
2024-12-17 21:29:41 +0330

    fix: fix token on step two

--- src/containers/PaymentGatewayMultiStep/network.ts
+++ src/containers/PaymentGatewayMultiStep/network.ts
@@ -1,41 +1,41 @@
 import { OptionType } from '../../models';
 
-export const networks: OptionType[] = [
-  {
+import stellarNetworkLogo from '../../../public/images/stellar.svg';
+
+export interface ITokensByNetwork {
+  network: OptionType;
+  tokens: OptionType[];
+}
+
+export const networks = {
+  stellar: {
     value: 'stellar',
     label: 'Stellar',
+    logo: stellarNetworkLogo,
+  },
+};
+
+const tokens = {
+  usdt: {
+    value: 'usdt',
+    label: 'USDT',
+    logo: 'https://s2.coinmarketcap.com/static/img/coins/64x64/825.png',
+  },
+  usdc: {
+    value: 'usdc',
+    label: 'USDC',
+    logo: 'https://s2.coinmarketcap.com/static/img/coins/64x64/3408.png',
+  },
+  xlm: {
+    value: 'xlm',
+    label: 'XLM',
     logo: 'https://s2.coinmarketcap.com/static/img/coins/64x64/512.png',
   },
+};
+
+export const tokensByNetwork: ITokensByNetwork[] = [
   {
-    value: 'ethereum',
-    label: 'Ethereum',
-    logo: 'https://s2.coinmarketcap.com/static/img/coins/64x64/1027.png',
+    network: networks.stellar,
+    tokens: [tokens.xlm, tokens.usdt, tokens.usdc],
   },
 ];
-
-export const tokensByNetwork: Record<string, OptionType[]> = {
-  stellar: [
-    {
-      value: 'usdt',
-      label: 'USDT',
-      logo: 'https://s2.coinmarketcap.com/static/img/coins/64x64/825.png',
-    },
-    {
-      value: 'usdc',
-      label: 'USDC',
-      logo: 'https://s2.coinmarketcap.com/static/img/coins/64x64/3408.png',
-    },
-  ],
-  ethereum: [
-    {
-      value: 'eth',
-      label: 'ETH',
-      logo: 'https://s2.coinmarketcap.com/static/img/coins/64x64/1027.png',
-    },
-    {
-      value: 'dai',
-      label: 'DAI',
-      logo: 'https://s2.coinmarketcap.com/static/img/coins/64x64/4943.png',
-    },
-  ],
-};
