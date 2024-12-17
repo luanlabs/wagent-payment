42337c9f36ffc5a0ee09ea5485607bc4e8812cd1
dousdogukan <dousdogukan@gmail.com>
2024-12-17 21:27:39 +0330

    chore: add generateQr code and memo

--- src/containers/Steps/StepThree/index.tsx
+++ src/containers/Steps/StepThree/index.tsx
@@ -1,39 +1,104 @@
+import { useEffect, useState } from 'react';
+
 import StepWrapper from '../../../components/StepWrapper';
+
+import { useAppSelector } from '../../../hooks/useRedux';
+
+import copyText from '../../../utils/copyText';
+import generateQrCode from '../../../utils/generateQrCode';
+
+import { ITokenResponse, OptionType } from '../../../models';
 
 import Copy from '../../../assets/Copy';
 
-import { OptionType } from '../../../models';
-import copyText from '../../../utils/copyText';
-
 interface StepThreeProps {
   prevStep: () => void;
+  setStep: (_: number) => void;
   selectedNetwork: OptionType | null;
-  selectedToken: OptionType | null;
+  selectedToken: ITokenResponse | null;
+  setIsPayed: (_: boolean) => void;
 }
 
-const StepThree = ({ prevStep, selectedNetwork, selectedToken }: StepThreeProps) => {
+const StepThree = ({
+  prevStep,
+  selectedNetwork,
+  selectedToken,
+  setStep,
+  setIsPayed,
+}: StepThreeProps) => {
+  const [qrCode, setQrCode] = useState<string | null>(null);
+
+  const { data, loading } = useAppSelector((store) => store.data);
+
+  let amount = '0';
+  if (!loading && data) {
+    amount =
+      data.tokens.find((token) => token.token.symbol === selectedToken?.symbol)?.amount || '0';
+
+    if (data.status === 'completed') {
+      setIsPayed(true);
+      setStep(4);
+    }
+  }
+
+  const paymentAddress = import.meta.env.VITE_CONTRACT_ADDRESS;
+
+  const generateQr = async () => {
+    try {
+      const qr = await generateQrCode(paymentAddress);
+      setQrCode(qr);
+    } catch (error) {
+      console.error('Error generating QR code:', error);
+    }
+  };
+
+  useEffect(() => {
+    generateQr();
+  }, []);
+
   const handleCopyAddress = () => {
-    copyText('fkjhfkajshfkasldjh');
+    copyText(paymentAddress);
+  };
+
+  const handleCopyMemo = () => {
+    copyText(data?.memo || '');
   };
 
   return (
     <StepWrapper title="Pay for your order" prevStep={prevStep} showNext={false}>
-      <div className="mt-5 flex flex-col items-center">
+      <div className="mt-0 flex flex-col items-center">
         <h2 className="font-medium text-[#101828] font-[Aeonik-m]">
-          Please send <span className="text-green-500 font-[Aeonik-b]">550.99 USDT</span> to Address
-          below
+          Please send{' '}
+          <span className="text-green-500 font-[Aeonik-b]">
+            {amount} {selectedToken?.symbol.toUpperCase()}{' '}
+          </span>
+          to Address below
         </h2>
-        <div className="w-[172px] h-[172px] overflow-hidden border border-1 border-[#E4E7EC] rounded-xl mt-3">
-          <img
-            src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d0/QR_code_for_mobile_English_Wikipedia.svg/800px-QR_code_for_mobile_English_Wikipedia.svg.png"
-            alt="qrCode"
-            className="object-cover w-full h-full"
-          />
+        <div className="w-[142px] h-[142px] overflow-hidden border border-1 border-[#E4E7EC] rounded-xl mt-3">
+          {qrCode ? (
+            <img src={qrCode} alt="qrCode" className="object-cover w-full h-full scale-[1.115]" />
+          ) : (
+            'Error'
+          )}
         </div>
 
-        <section className="flex flex-col border border-1 border-[#FEDF89] rounded-xl w-[450px] mobile:w-full mt-4">
+        <div className="flex justify-between border border-1 border-[#E4E7EC] mt-3 rounded-md">
+          <div className="py-1 px-3 mobile:text-[14px]">
+            Memo:<span className="ml-1 font-[Aeonik-m]">{data?.memo}</span>
+          </div>
+          <div
+            className="border-l border-1 border-[#E4E7EC] flex justify-center items-center py-2 px-3 cursor-pointer"
+            onClick={handleCopyMemo}
+          >
+            <Copy fill="#667085" />
+          </div>
+        </div>
+
+        <section className="flex flex-col border border-1 border-[#FEDF89] rounded-xl w-[450px] mobile:w-full mt-3">
           <div className="flex justify-between border-r-0 border-b border-1 border-[#FEDF89]">
-            <div className="py-2 px-3 mobile:text-[14px]">TDiScJc1jOdLTVvzysSnNC3R2Z316ZbMg4</div>
+            <div className="py-2 px-3 mobile:text-[14px]">
+              <span className="break-all text-[15px]">{paymentAddress}</span>
+            </div>
             <div
               className="border-l border-1 border-[#FEDF89] flex justify-center items-center py-2 px-3 cursor-pointer"
               onClick={handleCopyAddress}
@@ -47,7 +112,7 @@ const StepThree = ({ prevStep, selectedNetwork, selectedToken }: StepThreeProps)
               Pay only in
               <div className="text-[#475467] flex items-center font-[Aeonik-m] space-x-1 mx-1">
                 <img src={selectedToken?.logo} draggable={false} width={16} height={16} />
-                <p>{selectedToken?.label}</p>
+                <p>{selectedToken?.symbol.toUpperCase()}</p>
               </div>
               on
               <div className="text-[#475467] flex items-center font-[Aeonik-m] space-x-1 !mx-1">
